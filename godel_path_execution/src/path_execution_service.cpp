6d4f65235379814c10ea2db0c732fa75a615f375
Jonathan Meyer <jonathan.meyer@swri.org>
2015-07-01 18:17:32 -0500

    Path execution - Removed simulation branch. Need to modify godel_msgs

--- godel_path_execution/src/path_execution_service.cpp
+++ godel_path_execution/src/path_execution_service.cpp
@@ -31,55 +31,35 @@ bool godel_path_execution::PathExecutionService::executionCallback(godel_msgs::T
 
   ROS_INFO_STREAM("PathExecutionService '" << name_ << "': recieved new trajectory (simulate ==" << (req.simulate ? "true" : "false") << ")");
 
-  if (req.trajectory.points.empty()) {
+  if (req.trajectory.points.empty()) 
+  {
     ROS_WARN_STREAM("Cannot execute path with no trajectory points. Ignoring");
-    return true;
+    return false;
   }
 
-  if (req.simulate)
+  if (!ac_.waitForServer(ros::Duration(2.0)))
   {
-    // // Pass the trajectory to the simulation service
-    // SimulateTrajectory srv;
-    // srv.request.wait_for_execution = req.wait_for_execution;
-    // srv.request.trajectory = req.trajectory;
-
-    // if (sim_client_.call(srv))
-    // {
-    //   // currently no response fields in the simulate header
-    //   return true;
-    // }
-    // ROS_WARN_STREAM("PathExecutionService: Failed to call simulation service");
+    ROS_ERROR_STREAM("Could not connect to action server");
+    return false;
   }
-  else
-  {
-    // Pass the trajectory to moveit
-    // ExecuteKnownTrajectory srv;
-    // srv.request.wait_for_execution = req.wait_for_execution;
-    // srv.request.trajectory.joint_trajectory = req.trajectory;
 
-    // if (real_client_.call(srv))
-    // {
-    //   res.code = srv.response.error_code.val;
-    //   return true;
-    // }
-    // ROS_WARN_STREAM("PathExecutionService: Failed to call hardware execution service");
-    if (!ac_.waitForServer(ros::Duration(2.0)))
+  control_msgs::FollowJointTrajectoryGoal goal;
+  goal.trajectory = req.trajectory;
+  ROS_WARN_STREAM("Sending action server goal");
+  ac_.sendGoal(goal);
+
+  if (req.wait_for_execution)
+  {
+    if (ac_.waitForResult(goal.trajectory.points.back().time_from_start + ros::Duration(1.0)))
     {
-      ROS_ERROR_STREAM("Could not connect to action server");
+      return ac_.getState().state_ == ac_.getState().SUCCEEDED;
+    }
+    else
+    {
+      ROS_WARN_STREAM(__FUNCTION__ << ": Did not complete goal in time");
       return false;
     }
-
-    control_msgs::FollowJointTrajectoryGoal goal;
-    goal.trajectory = req.trajectory;
-    ROS_WARN_STREAM("Sending action server goal");
-    ac_.sendGoal(goal);
-
-    if (req.wait_for_execution && ac_.waitForResult(goal.trajectory.points[goal.trajectory.points.size()-1].time_from_start))
-    {
-      ROS_INFO_STREAM("Action server reported successful execution");
-    }
-    return true;
   }
 
-  return false;
+  return true;
 }
