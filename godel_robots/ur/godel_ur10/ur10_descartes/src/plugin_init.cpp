74dcbdccd81b133cce884400589ecfdacc0604b6
Jonathan Meyer <jonathan.meyer@swri.org>
2015-08-17 18:03:27 -0500

    Added basic UR5 and UR10 robot implementations. Needs calibration, testing, etc... still but the kinematics look reasonable

---
+++ godel_robots/ur/godel_ur10/ur10_descartes/src/plugin_init.cpp
@@ -0,0 +1,10 @@
+/*
+ * plugin_init.cpp
+ *
+ *  Created on: Apr 15, 2015
+ *      Author: Jorge Nicho
+ */
+
+#include <ur10_descartes/ur10_robot_model.h>
+#include <pluginlib/class_list_macros.h>
+PLUGINLIB_EXPORT_CLASS(ur10_descartes::UR10RobotModel, descartes_core::RobotModel)
